753ebc23424d4e378c3a0165e75c856ff1d0eff5
Bishesh <bishesh.sunam@gmail.com>
2020-12-26 00:16:32 +1300

    styled header & sidebar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import '../Components/Header.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
         <div className='header'>
+          <div className="header__left">
+            <IconButton>
+                <MenuIcon />
+            </IconButton>
+            <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="logo"/>
+          </div>
+          <div className="header__middle">
+            <IconButton>
+                <SearchIcon />
+            </IconButton>
+            <input placeholder="Search mail" type="text"/>
+            <IconButton>
+                <ArrowDropDownIcon />
+            </IconButton>
+            
+          </div>
+          <div className="header__right">
+              <IconButton>
+                  <AppsIcon />
+              </IconButton>
+              <IconButton>
+                  <NotificationsIcon />
+              </IconButton>
+              <Avatar />
+
+          </div>
           
         </div>
     )
