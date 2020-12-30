3e965de97b5cec9ec1c3d163663540e0e61855a5
Bishesh <bishesh.sunam@gmail.com>
2020-12-30 23:06:07 +1300

    added email body

--- src/Components/EmailRow.js
+++ src/Components/EmailRow.js
@@ -3,10 +3,13 @@ import React from 'react';
 import '../Components/EmailRow.css';
 import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 import LabelImportantOutlinedIcon from '@material-ui/icons/LabelImportantOutlined';
+import { useHistory } from 'react-router-dom';
 
 function EmailRow({ id, title, subject, description, time }) {
+
+    const history = useHistory();
     return (
-        <div className='emailRow'>
+        <div onClick={() => history.push("/mail")} className='emailRow'>
             <div className="emailRow__options">
                 <Checkbox />
                 <IconButton>
